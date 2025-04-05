57946633970685b4dc5a9df56a33a112756538a9
Siddarth Banerjee <siddarth2801@gmail.com>
2025-04-06 00:45:08 +0530

    Login page UI done

--- frontend/budget-tracker/src/App.jsx
+++ frontend/budget-tracker/src/App.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}></Route>
+          <Route path="/login" exact element={<Login />}></Route>
+          <Route path="/signUp" exact element={<SignUp />}></Route>
+          <Route path="/dashboard" exact element={<Home />}></Route>
+          <Route path="/income" exact element={<Income />}></Route>
+          <Route path="/expense" exact element={<Expense />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
+
+const Root = () => {
+  // Checking if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
